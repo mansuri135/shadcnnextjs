f2b7caad8a250a6a965fcaa1a1f5510123196443
minhaj135 <minhaj.mansuri9298@gmail.com>
2024-04-05 12:26:24 +0530

    commited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { AlertDialogDemo } from "@/components/alert/alert";
 import { CardWithForm } from "@/components/card/card";
 import { AccordionDemo } from "@/components/comp/data";
+import { ProgressDemo } from "@/components/progressbar/progresh";
 import Image from "next/image";
 
 export default function Home() {
@@ -17,7 +18,9 @@ export default function Home() {
   <div className="ml-40 mr-40 mt-10">
     <CardWithForm />
   </div>
-  
+  <div className="ml-40 mr-40 mt-10">
+    <ProgressDemo />
+  </div>
     </>
   );
 }
